8b63365d5a699523fe3a6b1b98ac41edafda6643
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2025-04-15 02:46:44 -0700

    Revert some of the suggested changes that do not work

--- modules/global_load_balancer/main.tf
+++ modules/global_load_balancer/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 resource "google_compute_global_forwarding_rule" "https" {
-  provider              = var.provider_global
+  provider              = google.global
   name                  = "global-https-forwarding-rule"
   project               = var.project_id
   target                = google_compute_target_https_proxy.https.id
@@ -18,7 +18,7 @@ resource "google_compute_global_forwarding_rule" "https" {
 }
 
 resource "google_compute_target_https_proxy" "https" {
-  provider = var.provider_global
+  provider = google.global
   name     = "global-https-target-proxy"
   project  = var.project_id
   url_map  = google_compute_global_url_map.default.id
@@ -29,14 +29,14 @@ resource "google_compute_target_https_proxy" "https" {
 # name of the resource provisioned?
 # Also, why is there another name attribute?
 resource "google_compute_global_url_map" "default" {
-  provider        = var.provider_global
+  provider        = google.global
   name            = "global-url-map"
   project         = var.project_id
   default_service = google_compute_global_backend_service.default.id
 }
 
 resource "google_compute_global_backend_service" "default" {
-  provider              = var.provider_global
+  provider              = google.global
   name                  = "global-backend-service"
   project               = var.project_id
   protocol              = "HTTP" # Or HTTPS if traffic to backends is also secured
--- modules/global_load_balancer/variables.tf
+++ modules/global_load_balancer/variables.tf
@@ -32,8 +32,3 @@ variable "health_check_port" {
 #   description = "Path to the SSL private key file"
 #   default     = ""
 # }
-
-variable "provider_global" {
-  type        = any
-  description = "The google provider configuration with the 'global' alias"
-}
